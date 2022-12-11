96bffbbb7bd163daccca5ef4f9b5e7ffdde78fee
David <github@dankme.me.uk>
2022-12-11 13:55:40 +0100

    Create main.py
    Eksempel på et tomt vindue i Tkinter

---
+++ Grafiske Brugergrænseflader/Tkinter/Tomt_vindue/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+##
+# © David Lindholm - me@davidlindholm.info
+# License: GNU Affero General Public License v3.0
+# Legal: https://www.gnu.org/licenses/agpl-3.0.en.html
+##
+
+##
+# Eksempel på det simpleste Tkinter program: Et tomt vindue
+##
+
+import tkinter as tk
+
+# Generelle variable kan erklæres her, men ikke elementer i vinduet
+
+vindue = tk.Tk() # Opret det grundlæggende vindueselement
+
+# Alt indhold i vinduet skrives på dette punkt i koden
+
+vindue.mainloop() # Mainloop sørger for at vinduet åbner og forbliver åbent
